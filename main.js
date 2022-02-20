6769b7c7a9c5397de9433a04bebce111543e5ffb
Adhham Safwan <msmadhham@outlook.com>
2022-02-20 13:33:05 +0530

    Format

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
 // Settings
 let customizeBtn = document.querySelector('.fa-cog');
 customizeBtn.addEventListener('click', () => {
-    let customizeBox = document.querySelector('.settings-box');
-    if(customizeBox.style.display === 'grid') {
-        customizeBox.style.display = 'none';
-    } else {
-        customizeBox.style.display = 'grid';
-    }
+  let customizeBox = document.querySelector('.settings-box');
+  if (customizeBox.style.display === 'grid') {
+    customizeBox.style.display = 'none';
+  } else {
+    customizeBox.style.display = 'grid';
+  }
 });
 //  Brightness set
 let brightnessSelector = document.getElementById('brightness');
 brightnessSelector.addEventListener('input', () => {
-    document.body.style = `filter: brightness(${brightnessSelector.value}%);`;
+  document.body.style = `filter: brightness(${brightnessSelector.value}%);`;
 });
 // Hover Change
-let hoverEls = document.querySelectorAll('.fa-user-circle, .fa-question-circle');
+let hoverEls = document.querySelectorAll(
+  '.fa-user-circle, .fa-question-circle'
+);
 for (let el of hoverEls) {
-    el.addEventListener('mouseover', () => {
-        el.classList.contains('fas') ? el.classList.replace('fas', 'far') : el.classList.replace('far', 'fas');
-    });
-    el.addEventListener('mouseout', () => {
-        el.classList.contains('fas') ? el.classList.replace('fas', 'far') : el.classList.replace('far', 'fas');
-    });
+  el.addEventListener('mouseover', () => {
+    el.classList.contains('fas')
+      ? el.classList.replace('fas', 'far')
+      : el.classList.replace('far', 'fas');
+  });
+  el.addEventListener('mouseout', () => {
+    el.classList.contains('fas')
+      ? el.classList.replace('fas', 'far')
+      : el.classList.replace('far', 'fas');
+  });
+}
+// About
+let aboutBtn = document.querySelectorAll('.fa-user-circle');
+for (let el of aboutBtn) {
+  el.addEventListener('click', () => {});
 }
